4e1ec57063b6a48d4ba48019c9dc57d5046aa5b9
jpratibha <jpratibha@vmware.com>
2024-06-21 08:29:01 +0530

    Payment service backstone project

---
+++ paymentservicefinal-master/src/main/java/com/scaler/paymentservicefinal/services/paymentgateway/RazorpayPaymentGateway.java
@@ -0,0 +1,51 @@
+package com.scaler.paymentservicefinal.services.paymentgateway;
+
+import com.razorpay.PaymentLink;
+import org.springframework.stereotype.Service;
+import org.json.JSONObject;
+import com.razorpay.Payment;
+import com.razorpay.RazorpayClient;
+import com.razorpay.RazorpayException;
+
+import java.time.Instant;
+
+@Service
+public class RazorpayPaymentGateway implements PaymentGateway {
+    private RazorpayClient razorpayClient;
+
+    public RazorpayPaymentGateway(RazorpayClient razorpayClient) {
+        this.razorpayClient = razorpayClient;
+    }
+
+    @Override
+    public String generatePaymentLink(String orderId, String email, String phoneNumber, Long amount) {
+        try {
+            JSONObject paymentLinkRequest = new JSONObject();
+            paymentLinkRequest.put("amount",amount);
+            paymentLinkRequest.put("currency","INR");
+            paymentLinkRequest.put("accept_partial",false);
+            paymentLinkRequest.put("expire_by", 1733558886);
+            paymentLinkRequest.put("reference_id",orderId);
+            paymentLinkRequest.put("description","Payment for order #" + orderId);
+            JSONObject customer = new JSONObject();
+            customer.put("name","Pratibha Jain");
+            customer.put("contact",phoneNumber);
+            customer.put("email",email);
+            paymentLinkRequest.put("customer",customer);
+            JSONObject notify = new JSONObject();
+            notify.put("sms",true);
+            notify.put("email",true);
+            paymentLinkRequest.put("notify",notify);
+            paymentLinkRequest.put("reminder_enable",true);
+            paymentLinkRequest.put("callback_url","https://example-callback-url.com/");
+            paymentLinkRequest.put("callback_method","get");
+
+            PaymentLink payment = razorpayClient.paymentLink.create(paymentLinkRequest);
+//            return payment.toString();
+            return payment.get("short_url").toString();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            return "something is wrong";
+        }
+    }
+}
---
+++ paymentservicefinal-master/src/test/java/com/scaler/paymentservicefinal/PaymentservicefinalApplicationTests.java
@@ -0,0 +1,15 @@
+package com.scaler.paymentservicefinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration("/applicationContext.xml")
+class PaymentservicefinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
